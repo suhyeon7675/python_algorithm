a79260fe26c927e1a38c90080327b6deeb999645
skytmddus27 <skytmddus27@gmail.com>
2023-04-25 12:32:44 +0900

    2023-04-24

---
+++ 2023-04-24/다음에 올 숫자/3_이트.py
@@ -0,0 +1,6 @@
+def solution(common):
+    if common[1] - common[0] == common[2] - common[1]:
+        answer = common[-1] + (common[1] - common[0])
+    else:
+        answer = common[-1] * (common[1] / common[0])
+    return answer
