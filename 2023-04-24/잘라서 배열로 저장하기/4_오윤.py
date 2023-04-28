0addf66fcfbe17bbce994f75ed34ad64d83bf4ba
Ryujy <63836145+Ryujy@users.noreply.github.com>
2023-04-29 02:37:12 +0900

    Update 4_오윤.py

--- 2023-04-24/잘라서 배열로 저장하기/4_오윤.py
+++ 2023-04-24/잘라서 배열로 저장하기/4_오윤.py
@@ -1,3 +1,4 @@
+# [PRG]잘라서 배열로 저장하기
 def solution(my_str, n):
     answer = []
     
@@ -5,8 +6,11 @@ def solution(my_str, n):
         if len(my_str) > n:  # 잘라내는 동안 my_str의 길이가 n보다 작아지면 인덱스 에러가 발생할 수도 있으므로 조건문으로 상황을 나눠준다.
             answer.append(my_str[:n])  # 슬라이싱으로 n개씩 자르기
             my_str = my_str[n:]  # 슬라이싱으로 answer에 추가한 부분 삭제
-        else:
+        else:                       
             answer.append(my_str)
             break
 
     return answer
+
+
+# TIL : 슬라이싱을하면 인덱스가 len(my_str)을 초과하는 경우가 생길거라 생각했는데 슬라이싱은 초과해도 에러가 나지 않는다고 한다...!!!!
