027c4eb33139492ecd7c466ee013b8840af550bd
99999xwx9 <95744779+99999xwx9@users.noreply.github.com>
2023-05-07 21:15:38 +0900

    Create 1_당근.py

---
+++ 2023-05-01/문자열 정렬하기 (2)/1_당근.py
@@ -0,0 +1,9 @@
+def solution(my_string):
+    
+    my_string = my_string.lower()
+    my_string = sorted(my_string)
+    for i in my_string :
+        print(i, end = '')
+    anwser = i
+
+    return anwser
