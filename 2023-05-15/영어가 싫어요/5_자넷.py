da1e1a28a781d15586fabe28888acddbc6b0e43f
janetchu05 <82367137+janetchu05@users.noreply.github.com>
2023-05-18 10:34:01 +0900

    Update hateEnglish by Janet on 230518

--- 2023-05-15/영어가 싫어요/5_자넷.py
+++ 2023-05-15/영어가 싫어요/5_자넷.py
@@ -1,5 +1,8 @@
-def solution(my_string, num1, num2):
-    answer = list(my_string)
-    answer[num1], answer[num2] = answer[num2], answer[num1]
+def solution(numbers):
+    numbWords = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+    for i in range(0, len(numbWords)):
+        if numbWords[i] in numbers:
+            numbers = numbers.replace(numbWords[i], str(i))
     
-    return ''.join(answer)
+    return int(numbers)
