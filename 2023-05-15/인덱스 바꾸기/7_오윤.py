f20e8d86ab2f1144f437a95129e6e49f2e3bacd7
Ryujy <63836145+Ryujy@users.noreply.github.com>
2023-05-21 15:38:18 +0900

    Update 7_오윤.py

--- 2023-05-15/인덱스 바꾸기/7_오윤.py
+++ 2023-05-15/인덱스 바꾸기/7_오윤.py
@@ -7,7 +7,7 @@ def solution(my_string, num1, num2):
     return(''.join(my_string))
   
 # 방법은 같은데 더 간단한 코드
-# 5줄 -> 3줄
+# 5줄 -> 2줄
 def solution(my_string, num1, num2):
     s = list(my_string)
     s[num1] , s[num2] = s[num2] , s[num1] 
